edc5ba601dec146a6b510683180430e88cf25768
yosBoi <y.upneja@gmail.com>
2020-05-27 00:16:47 +0530

    removed note shema dependency error

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const Note = require('../models/Note');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User')
 
