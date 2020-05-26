9c3fde9e2dedc423218007c81b0c22542ddd0603
yosBoi <y.upneja@gmail.com>
2020-05-27 02:02:06 +0530

    added basic loading animation

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -24,8 +24,19 @@ export const AuthProvider = (props) => {
   }, []);
 
   return (
+    //return loading animation until loading is done at beginning
     <div>
-      {!isLoaded ? <h1>Loading</h1> : 
+      {!isLoaded ? 
+      <div class="spinner">
+        <div class="rect1"></div>
+        <div class="rect2"></div>
+        <div class="rect3"></div>
+        <div class="rect4"></div>
+        <div class="rect5"></div>
+      </div> 
+      
+      :
+
       <AuthContext.Provider value = {{user, setUser, isAuthenticated, setIsAuthenticated}}>
         {props.children}
       </AuthContext.Provider>
