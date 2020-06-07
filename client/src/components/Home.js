b0154445ad6c9cb411488dd390bf6770848883cc
yosBoi <y.upneja@gmail.com>
2020-06-07 08:11:17 +0530

    added github link

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -9,18 +9,24 @@ const Home = props => {
 
   if(isAuthenticated){
     return(
+      <>
       <div className="menu-redirect">
         <h3>Welcome, {user}</h3>
         <p>To access your notes, go to the <Link to="/notes">Notes section</Link></p>
       </div>
+      <footer><a href="https://github.com/yosBoi/url-shortener">Github <i class="fab fa-github"></i></a></footer>
+      </>
     )
   }
   else{
     return(
+      <>
       <div className="menu-redirect">
         <h3>To log in, go to the <Link to="/login">Login page</Link></h3>
         <h3>To register, go to the <Link to="/register">Register page</Link></h3>
       </div>
+      <footer><a href="https://github.com/yosBoi/url-shortener">Github <i class="fab fa-github"></i></a></footer>
+      </>
     )
   }
 }
